9835040d2e1ab8e7a5b99a7b4f4e4e84ce82218f
GortariCL <fernando.gortari@gmail.com>
2023-12-20 15:20:25 -0300

    some changes

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -3,12 +3,14 @@ import "./ItemDetailContainer.css";
 
 export const ItemDetailContainer = ({ nombre, precioCLP, imagen }) => {
   return (
-    <div className="item__detail">
-      <div className="img__frame">
-        <img className="img__crop" src={imagen} alt={nombre} />
+    <div className="item__detail__container">
+      <div className="item__detail">
+        <div className="img__frame">
+          <img className="img__crop" src={imagen} alt={nombre} />
+        </div>
+        <h2>{nombre}</h2>
+        <p>Precio: ${precioCLP}</p>
       </div>
-      <h2>{nombre}</h2>
-      <p>Precio: ${precioCLP}</p>
     </div>
   );
 };
